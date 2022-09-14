5ef6161deeca6734ee0863f72630f1db93c6a5d9
Kevin Templeton <templeton.kevin@gmail.com>
2022-09-14 16:23:34 -0500

    complete

--- app.py
+++ app.py
@@ -1,13 +1,73 @@
+"""Flask app for Cupcakes"""
+
 from flask import Flask, request, jsonify, render_template
-from models import db, connect_db
+from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///***'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "oh-so-secret"
 
 connect_db(app)
 
+@app.route("/")
+def root():
+    """Render homepage."""
 
-@app.route('/')
+    return render_template("index.html")
+
+@app.route('/api/cupcakes')
+def list_cupcakes():
+    """Get data about all cupcakes."""
+
+    cupcakes = [cupcake.to_dict() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=cupcakes)
+
+@app.route('/api/cupcakes/<int:cupcake_id>')
+def get_cupcake(cupcake_id):
+    """Get data about one cupcake."""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    return jsonify(cupcake=cupcake.to_dict())
+
+@app.route('/api/cupcakes', methods=['POST'])
+def create_cupcake():
+    """Add new cupcake and then return its data."""
+
+    data = request.json
+    cupcake = Cupcake(flavor=data['flavor'], rating=data['rating'], size=data['size'], image=data['image'] or None)
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    return (jsonify(cupcake=cupcake.to_dict()), 201)
+
+@app.route("/api/cupcakes/<int:cupcake_id>", methods=["PATCH"])
+def update_cupcake(cupcake_id):
+    """Update cupcake and then return updated data."""
+
+    data = request.json
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    cupcake.flavor = data['flavor']
+    cupcake.rating = data['rating']
+    cupcake.size = data['size']
+    cupcake.image = data['image']
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    return jsonify(cupcake=cupcake.to_dict())
+
+
+@app.route("/api/cupcakes/<int:cupcake_id>", methods=["DELETE"])
+def remove_cupcake(cupcake_id):
+    """Delete cupcake and return confirmation message."""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    db.session.delete(cupcake)
+    db.session.commit()
+
+    return jsonify(message="Deleted")
